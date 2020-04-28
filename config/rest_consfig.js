83c984d6abb73e7b1d34e1c52762c589d8e6cdc8
VEPTHSoc <root@localhost.localdomain>
2020-04-28 17:25:28 +0530

    final commit

---
+++ config/rest_consfig.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'in';
+export const category = 'general';
+export const _api_key = 'd4c2b6ecae8d4bbdb5a4954b92d11f89';
